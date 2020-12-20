35d89d5c909f518c5c55d6ee24b902139e68e8a2
Anno Langen <anno.langen@gmail.com>
2020-12-20 12:14:40 -0800

    Added unit test support.

---
+++ src/ToStringTest.cc
@@ -0,0 +1,15 @@
+#include "ToString.h"
+#include "testing/catch.h"
+
+SCENARIO("ToString produces strings for all our classes", "[ToString]") {
+
+  GIVEN("A RecordType") {
+    RecordType record_type({{"x", "string"}, {"y", "int"}});
+    REQUIRE(ToString(record_type) == "{x: string, y: int}");
+    WHEN("visited") {
+      TypeVisitor visitor;
+      record_type.Accept(visitor);
+      THEN("ToString is unaffected") { REQUIRE(ToString(record_type) == "{x: string, y: int}"); }
+    }
+  }
+}
--- src/tc.cc
+++ src/tc.cc
@@ -4,10 +4,5 @@
 
 int main(int argc, char **argv) {
   std::cout << "I compile Tiger" << std::endl;
-
-  std::cout << "Test type "
-            << ToString(RecordType({{"x", "string"}, {"y", "int"}}))
-            << std::endl;
-
   return 0;
 }
