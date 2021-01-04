ab149792890b1beff22f4fd44b629484abcd48a3
Anno Langen <anno.langen@gmail.com>
2021-01-04 10:25:32 -0800

    Refactor emit test.

--- src/emitTest.cc
+++ src/emitTest.cc
@@ -16,39 +16,42 @@ std::string ReadFile(const char* path) {
   std::ifstream t(path);
   return {std::istreambuf_iterator<char>(t), std::istreambuf_iterator<char>()};
 }
+
+// Finishes the given instructions for the method body of "main" with a return
+// statement, defines the "main" method using these instructions, and outputs
+// the resulting program as class file /tmp/Main.class.
+void EmitAsMain(std::ostringstream& main_instructions, Program& program) {
+  main_instructions.put(Instruction::_return);
+  program.DefineFunction(emit::ACC_PUBLIC | emit::ACC_STATIC, "main",
+                         "([Ljava/lang/String;)V", main_instructions.str());
+  std::ofstream out("/tmp/Main.class");
+  program.Emit(out);
+}
+
 SCENARIO("emits class file", "[emit]") {
   GIVEN("Hello World") {
     const char* msg = "Hello, World!\n";
     auto program = Program::JavaProgram();
     if (auto f = program->LookupLibraryFunction("print"); f) {
       auto text = program->DefineStringConstant(msg);
-      std::ostringstream main_os;
-      f->Call(main_os, {text});
-      main_os.put(Instruction::_return);
-      program->DefineFunction(emit::ACC_PUBLIC | emit::ACC_STATIC, "main",
-                              "([Ljava/lang/String;)V", main_os.str());
-      std::ofstream out("/tmp/Main.class");
-      program->Emit(out);
+      std::ostringstream main_instructions;
+      f->Call(main_instructions, {text});
+      EmitAsMain(main_instructions, *program);
     } else {
       FAIL("Library function print not found");
     }
     REQUIRE(RunJava() == msg);
   }
-  
+
   GIVEN("printint") {
-    const int i = 20202020;
     auto program = Program::JavaProgram();
     if (auto f = program->LookupLibraryFunction("printi"); f) {
-      auto text = program->DefineIntegerConstant(i);
-      std::ostringstream main_os;
-      f->Call(main_os, {text});
-      main_os.put(Instruction::_return);
-      program->DefineFunction(emit::ACC_PUBLIC | emit::ACC_STATIC, "main",
-                              "([Ljava/lang/String;)V", main_os.str());
-      std::ofstream out("/tmp/Main.class");
-      program->Emit(out);
+      auto int_constant = program->DefineIntegerConstant(20202020);
+      std::ostringstream main_instructions;
+      f->Call(main_instructions, {int_constant});
+      EmitAsMain(main_instructions, *program);
     } else {
-      FAIL("Library function print not found");
+      FAIL("Library function printi not found");
     }
     REQUIRE(RunJava() == "20202020");
   }
