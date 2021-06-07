6e42460913e32bc92236cc5079801734bcfe94fb
Anno Langen <anno.langen@gmail.com>
2021-06-06 21:50:29 -0700

    Trying alternate, lighter syntax nodes.

---
+++ src/Token.h
@@ -0,0 +1,52 @@
+#pragma once
+
+enum Token {
+  EOF = 0,
+  AND,
+  ARRAY,
+  ASSIGN,
+  BREAK,
+  COLON,
+  COMMA,
+  DO,
+  DOT,
+  ELSE,
+  END,
+  EQUAL,
+  FOR,
+  FUNCTION,
+  GE,
+  GT,
+  IDENTIFIER,
+  IF,
+  IN,
+  LBRACE,
+  LBRACKET,
+  LE,
+  LET,
+  LPAREN,
+  LT,
+  MINUS,
+  NE,
+  NIL,
+  NUMBER,
+  OF,
+  OR,
+  PLUS,
+  RBRACE,
+  RBRACKET,
+  RPAREN,
+  SEMICOLON,
+  SLASH,
+  STAR,
+  STRING_CONSTANT,
+  THEN,
+  TO,
+  TYPE,
+  VAR,
+  WHILE
+};
+
+extern int yylex();
+extern char* yytext;
+extern int yylen;
