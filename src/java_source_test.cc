3cefa5e9fd66642b314be8f92c2c5cfa0b98f820
Anno Langen <anno.langen@gmail.com>
2026-04-25 12:53:59 -0700

    Fix tests

--- src/java_source_test.cc
+++ src/java_source_test.cc
@@ -3,6 +3,7 @@
 #include "catch2/catch_test_macros.hpp"
 #include "catch2/matchers/catch_matchers_string.hpp"
 #include "testing/testing.h"
+#include <algorithm>
 
 namespace {
 
@@ -17,7 +18,9 @@ std::string Compile(std::string_view text,
   std::vector<std::string> errors;
   TypeFinder tf(*st, errors);
   REQUIRE(errors.empty());
-  return java::Compile(*expr, *st, tf, class_name);
+  std::string result = java::Compile(*expr, *st, tf, class_name);
+  result.erase(std::remove(result.begin(), result.end(), '\r'), result.end());
+  return result;
 }
 
 SCENARIO("JavaSource") {
@@ -31,35 +34,35 @@ SCENARIO("JavaSource") {
   GIVEN("array type and an array variable") {
     REQUIRE_THAT(Compile(R"(
 let
-	type  arrtype = array of int
-	var arr1:arrtype := arrtype [10] of 0
+  type  arrtype = array of int
+  var arr1:arrtype := arrtype [10] of 0
 in
-	arr1
+  arr1
 end)"),
                  Equals(R"(import java.util.Arrays;
-  
+
 class Scope0 {
-  }
-  
+}
+
 class Scope1 {
-    public Scope0 parent;
-    public int[] arr1;
-  }
-  
+  public Scope0 parent;
+  public int[] arr1;
+}
+
 class Main {
-  
+
   public static void main(String[] args) {
-      Scope0 _scope0 = new Scope0();
-      {
-        Scope1 _scope1 = new Scope1();
-        _scope1.parent = _scope0;
-        _scope1.arr1 = new int[10];
-        Arrays.fill(_scope1.arr1, 0);
-        _scope1.arr1;
-      }
+    Scope0 _scope0 = new Scope0();
+    {
+      Scope1 _scope1 = new Scope1();
+      _scope1.parent = _scope0;
+      _scope1.arr1 = new int[10];
+      Arrays.fill(_scope1.arr1, 0);
+      _scope1.arr1;
     }
   }
-  )"));
+}
+)"));
   }
   GIVEN("8 Queens") {
     REQUIRE_THAT(Compile(R"(
@@ -87,84 +90,83 @@ let
 in try(0) end
 )"),
                  Equals(R"(import java.util.Arrays;
-  
+
 class Scope0 {
-  }
-  
+}
+
 class Scope1 {
-    public Scope0 parent;
-    public int[] diag2;
-    public int[] diag1;
-    public int[] row;
-    public int[] col;
-    public int N;
-  }
-  
+  public Scope0 parent;
+  public int[] diag2;
+  public int[] diag1;
+  public int[] row;
+  public int[] col;
+  public int N;
+}
+
 class Scope2 {
-    public Scope1 parent;
-  }
-  
+  public Scope1 parent;
+}
+
 class Scope3 {
-    public Scope1 parent;
-    public int c;
-  }
-  
+  public Scope1 parent;
+  public int c;
+}
+
 class Main {
-  
+
   public static void main(String[] args) {
-      Scope0 _scope0 = new Scope0();
-      {
-        Scope1 _scope1 = new Scope1();
-        _scope1.parent = _scope0;
-        _scope1.N = 8;
-        _scope1.row = new int[_scope1.N];
-        Arrays.fill(_scope1.row, 0);
-        _scope1.col = new int[_scope1.N];
-        Arrays.fill(_scope1.col, 0);
-        _scope1.diag1 = new int[_scope1.N + _scope1.N - 1];
-        Arrays.fill(_scope1.diag1, 0);
-        _scope1.diag2 = new int[_scope1.N + _scope1.N - 1];
-        Arrays.fill(_scope1.diag2, 0);
-        _try(_scope1, 0);
-      }
+    Scope0 _scope0 = new Scope0();
+    {
+      Scope1 _scope1 = new Scope1();
+      _scope1.parent = _scope0;
+      _scope1.N = 8;
+      _scope1.row = new int[_scope1.N];
+      Arrays.fill(_scope1.row, 0);
+      _scope1.col = new int[_scope1.N];
+      Arrays.fill(_scope1.col, 0);
+      _scope1.diag1 = new int[_scope1.N + _scope1.N - 1];
+      Arrays.fill(_scope1.diag1, 0);
+      _scope1.diag2 = new int[_scope1.N + _scope1.N - 1];
+      Arrays.fill(_scope1.diag2, 0);
+      _try(_scope1, 0);
     }
-  
+  }
+
       static void printboard(Scope1 _scope1) {
-          Scope2 _scope2 = new Scope2();
-          _scope2.parent = _scope1;
-          for (int i = 0; i <= _scope1.N - 1; i++) {
-            for (int j = 0; j <= _scope1.N - 1; j++) {
-              System.out.print((_scope1.col[i] == j ? " O" : " ."));
-            }
-            System.out.print("\n");
+        Scope2 _scope2 = new Scope2();
+        _scope2.parent = _scope1;
+        for (int i = 0; i <= _scope1.N - 1; i++) {
+          for (int j = 0; j <= _scope1.N - 1; j++) {
+            System.out.print((_scope1.col[i] == j ? " O" : " ."));
           }
           System.out.print("\n");
         }
-  
+        System.out.print("\n");
+      }
+
       static void _try(Scope1 _scope1, int c) {
-          Scope3 _scope3 = new Scope3();
-          _scope3.parent = _scope1;
-          _scope3.c = c;
-          if (_scope3.c == _scope1.N) {
-            printboard(_scope1);
-          } else {
-            for (int r = 0; r <= _scope1.N - 1; r++) {
-              if (_scope1.row[r] == 0 && _scope1.diag1[r + _scope3.c] == 0 &&
-  _scope1.diag2[r + 7 - _scope3.c] == 0) {
-                _scope1.row[r] = 1;
-                _scope1.diag1[r + _scope3.c] = 1;
-                _scope1.diag2[r + 7 - _scope3.c] = 1;
-                _scope1.col[_scope3.c] = r;
-                _try(_scope1, _scope3.c + 1);
-                _scope1.row[r] = 0;
-                _scope1.diag1[r + _scope3.c] = 0;
-                _scope1.diag2[r + 7 - _scope3.c] = 0;
-              }
+        Scope3 _scope3 = new Scope3();
+        _scope3.parent = _scope1;
+        _scope3.c = c;
+        if (_scope3.c == _scope1.N) {
+          printboard(_scope1);
+        } else {
+          for (int r = 0; r <= _scope1.N - 1; r++) {
+            if (_scope1.row[r] == 0 && _scope1.diag1[r + _scope3.c] == 0 && _scope1.diag2[r + 7 - _scope3.c] == 0) {
+              _scope1.row[r] = 1;
+              _scope1.diag1[r + _scope3.c] = 1;
+              _scope1.diag2[r + 7 - _scope3.c] = 1;
+              _scope1.col[_scope3.c] = r;
+              _try(_scope1, _scope3.c + 1);
+              _scope1.row[r] = 0;
+              _scope1.diag1[r + _scope3.c] = 0;
+              _scope1.diag2[r + 7 - _scope3.c] = 0;
             }
           }
         }
-  }
-  )"));
+      }
+}
+)"));
   }
 }
-}  // namespace
+} // namespace
